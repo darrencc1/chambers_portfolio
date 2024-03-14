6f2797696867136bff0d8db59e0abace17fdc13f
darrencc1 <darrencc@byui.edu>
2024-03-14 09:35:40 -0600

    commit

---
+++ Projects/Untitled-1.py
@@ -0,0 +1,4 @@
+#%%
+# copy paste this into a python file in vs code and run this cell
+import sys
+!{sys.executable} -m pip install numpy pandas scikit-learn plotly.express nbformat nbclient pyyaml
